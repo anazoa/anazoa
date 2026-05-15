3a793c43e5a416a46fa8cd51ca91f07e0d8a3b81
Ana Zoa <anazoa@proton.me>
2026-05-16 00:20:18 +0300

    Fix aarch64 build

--- scripts/build-arm64.sh
+++ scripts/build-arm64.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -eu
 
+# sudo dpkg --add-architecture arm64
+# sudo apt update
+# sudo apt install libopus-dev:arm64
+
 export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=aarch64-linux-gnu-gcc
 export PKG_CONFIG_SYSROOT_DIR=/
 export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
-export LK_CUSTOM_WEBRTC=webrtc-sys/libwebrtc/linux-arm64-release
+export LK_CUSTOM_WEBRTC=$(pwd)/webrtc-sys/libwebrtc/linux-arm64-release
 
 cargo build --release --target aarch64-unknown-linux-gnu
